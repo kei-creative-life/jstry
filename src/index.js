f7fb19ce93cc88e6e3d947b79c54678d9d8b4bb2
kei1992 <68884990+kei1992@users.noreply.github.com>
2020-10-18 17:59:16 +0900

    map&filter

--- src/index.js
+++ src/index.js
@@ -51,11 +51,87 @@
 // です`;
 // console.log(message2);
 
-// 分割代入　配列編
-const myProfile = ["john", 28];
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。}`;
-console.log(message3);
+// // 分割代入　配列編
+// const myProfile = ["john", 28];
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。}`;
+// console.log(message3);
 
-const [name, age] = myProfile;
-const message4 = `名前は${name}で、年齢は${age}です`;
-console.log(message4);
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}で、年齢は${age}です`;
+// console.log(message4);
+
+// // デフォルト値
+// nameが設定されていなければ,ゲストとするように設定。
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
+// sayHello("John");
+
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+// スプレッド構文 ...
+// 配列の展開
+// const arr1 = [1,2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// // スプレッド構文使わない
+// // const sumFunc = (num1,num2) => console.log(num1 + num2);
+// // sumFunc(arr1[0],arr1[1]);
+
+// // スプレッド構文使う場合
+// const sumFunc = (num1,num2) => console.log(num1 + num2);
+// sumFunc(...arr1);
+
+// まとめる：展開の反対
+// const arr2 = [1,2,3,4,5];
+// const [num1,num2,...arr3] = arr2;
+// console.log(num1);
+// console.log(arr3);
+
+// 配列のコピー
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr4);
+// console.log(arr6);
+
+// // 配列の結合
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+// map
+// 従来型：for文
+const nameArr = ["tanaka", "yamada", "honda"];
+// for (let index = 0; index < nameArr.length; index++){
+//   console.log(nameArr[index]);
+// }
+
+// map練習
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
+
+// mapなら簡潔！！
+// indexは第2引数
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// // filter：何か条件に合うやつを抜き出す
+// // 試しに奇数だけ取り出そう
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// 条件に応じて配列の中身を変える
+const newNameArr = nameArr.map((name) => {
+  if (name === "honda") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+
+console.log(newNameArr);
